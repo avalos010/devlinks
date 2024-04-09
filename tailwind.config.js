b304090c88e4a80d31624d83e5d6d5582be590e3
Jose Avalos <luizavalos40@gmail.com>
2024-04-09 18:58:28 -0500

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "purple-active": "#BEADFF",
+        "purple-default": "#633CFF",
+        "purple-disabled": "#EFEBFF",
+        "dark-gray": "#737373",
+        "light-gray": "#D9D9D9",
+        "super-light-gray": "#FAFAFA",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         btn: {
