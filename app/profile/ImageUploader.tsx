0924f74454b6fb001dab0b0299f12db23649f097
Jose Avalos <luizavalos40@gmail.com>
2024-05-14 21:07:54 -0500

    started working on profile image uploads

---
+++ app/profile/ImageUploader.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { redirect } from "next/navigation";
+import { ChangeEvent } from "react";
+
+function ImageUploader() {
+  const handleUploadImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    //TODO!: lets make this work with the server supabase client instead. Keep everything on server.
+    const file = e.target.files![0];
+    const supabase = createClient();
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    console.log(user?.id);
+
+    // Upload file using standard upload
+    const { data, error } = await supabase.storage
+      .from("Avatar")
+      .upload(`${user?.id}/profileImage`, file, { upsert: true }); //upsert: true means we could overwrite the image.
+    if (error) {
+      console.error(error.message); //TODO!: Handle ERROR
+    } else {
+      redirect("/profile"); //refresh page to show new profile image after successful upload;
+    }
+  };
+
+  return (
+    <input type="file" name="profilePicture" onChange={handleUploadImage} />
+  );
+}
+
+export default ImageUploader;
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,25 +1,42 @@
+//TODO!: Automatically create profile for user after signup.
+
 import { createClient } from "@/utils/supabase/server";
-import { protectedPage, supabase } from "@/utils/supabase/server-helpers";
-import { redirect } from "next/navigation";
+import { protectedPage } from "@/utils/supabase/server-helpers";
+import ImageUploader from "./ImageUploader";
+import Image from "next/image";
 
 export default async function ProtectedPage() {
   await protectedPage();
   const supabase = createClient();
 
-  const fetchUser = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  const { data, error } = await supabase
+  const { data: profile, error } = await supabase //Fetch Profile Details
     .from("profile")
     .select("*")
-    .eq("user_id", fetchUser.data.user?.id as string)
+    .eq("user_id", user?.id as string)
     .limit(1)
     .single();
+
+  //TODO! NOT WORKING
+  const { data } = await supabase.storage //Get Profile Picture
+    .from("Avatar")
+    .createSignedUrl(`${user?.id}/profileImage`, 300);
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       Profile
-      <p>{data?.first_name}</p>
-      <p>{data?.last_name}</p>
+      <Image
+        src={data?.signedUrl as string}
+        alt="profile image"
+        width={500}
+        height={500}
+      />
+      <ImageUploader />
+      <p>{profile?.first_name}</p>
+      <p>{profile?.last_name}</p>
     </div>
   );
 }
