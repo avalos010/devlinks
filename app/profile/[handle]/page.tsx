b0f2b8ad5e0c3ec473b9b0fafc28b6a7016ecce8
Jose Avalos <luizavalos40@gmail.com>
2024-05-21 17:03:05 -0500

    links showing up when visitin other user profile.

--- app/profile/[handle]/page.tsx
+++ app/profile/[handle]/page.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/services/supabaseServices";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
 
 const UserProfile = async ({ params }: { params: { handle: string } }) => {
   const userProfile = await getUserHandleProfile(params.handle);
@@ -16,7 +17,7 @@ const UserProfile = async ({ params }: { params: { handle: string } }) => {
   const imageUrl = await getProfileImageUrl(user_id as string);
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      Profile
+      <h2 className="text-2xl">@{handle}</h2>
       <Image
         src={imageUrl as string}
         alt="profile image"
@@ -26,6 +27,19 @@ const UserProfile = async ({ params }: { params: { handle: string } }) => {
       <p>
         {first_name} {last_name}
       </p>
+      <div className="flex flex-col gap-4">
+        {links?.map((link) => (
+          <a
+            className="bg-gray-600 text-white p-3 md:w-96 relative w-80"
+            key={link.id}
+            href={link.link}
+            target="_blank"
+          >
+            <p>{link.social}</p>
+            <FaArrowRight className="absolute right-9 top-5 cursor-pointer hover:scale-110" />
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
